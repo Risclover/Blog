f88ab85c7fa3b201d1e166168b66ff6262248e1e
Risclover <85785443+Risclover@users.noreply.github.com>
2022-01-17 20:21:22 -0800

    Images

---
+++ Template Files/script.js
@@ -0,0 +1,14 @@
+let categoryCounts = document.getElementById('categorycount');
+
+let cat1 = 0;
+let cat2 = 0;
+
+
+categoryCounts.forEach((count) => {
+    if (count > 0) {
+        
+    }
+})
+if (cat1 > 0) {
+    category
+}
